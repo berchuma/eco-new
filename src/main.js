718b92d4be0489648be4b5d546e2898da7555a59
Michael <memriasefer@gmail.com>
2023-10-05 21:09:51 +0100

    memo

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import App from "./App";
+import Cred from "./second-pae/credential/cred";
+const Main = () => {
+    return ( <div>
+      <div></div>
+        <BrowserRouter>
+        
+        <Routes>
+            <Route path="/" element={<App/>}/>
+            <Route path="/eco-new" element={<App/>}/>
+            <Route path="/new" element={<div>this is te new page</div>}/>
+            <Route path="/log" element={<Cred/>}/>
+        </Routes>
+        </BrowserRouter>
+    </div> );
+}
+ 
+export default Main;
